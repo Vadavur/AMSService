cc85c5e07ba80a7fd2a0d45ce8bd446d1386c814
Давыд Васильцов <65164004+Vadavur@users.noreply.github.com>
2021-02-26 21:20:27 +0300

    Delete scripts directory

--- scripts/loadJSON2.js
+++
@@ -1,9 +0,0 @@
-/* Загружает и передает содержимое JSON-файла */
-async function LoadJSON2(elementName, filePath) {
-  	return new Promise((resolve, reject) => {
-		let response = fetch(filePath);
-		let ans = response.json();
-		alert(response.status + '_' + response);
-   		ans.onload = () => resolve(ans);
-  	});
-}
--- scripts/loadJSON3.js
+++
@@ -1,7 +0,0 @@
-/* Загружает и передает содержимое JSON-файла */
-async function LoadJSON2(elementName, filePath) {
-	let response = await fetch(filePath);
-	let ans = await response.json();
-	alert(response.status + '_' + response);
-	return ans;
-}
